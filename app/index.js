2120e7eecd718abc8a31b5e0f6a6d7b57203818a
Ben Drucker <bvdrucker@gmail.com>
2015-04-25 21:25:44 -0400

    Import angular-credit-cards and angular-form-state

--- app/index.js
+++ app/index.js
@@ -1,12 +1,16 @@
 'use strict'
 
 import angular from 'angular'
+import formState from 'angular-form-state'
+import creditCards from 'angular-credit-cards'
 import backend from './backend'
 import stripe from './stripe'
 import Payment from './payment'
 import PaymentController from './controller'
 
 export default angular.module('payment', [
+  formState,
+  creditCards,
   backend,
   stripe
 ])
