b9ad9ca72a77259e8afd25edf3013d747cb307e3
Ben Drucker <bvdrucker@gmail.com>
2015-04-25 20:57:29 -0400

    Set up Stripe module

---
+++ stripe/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+import angular from 'angular'
+import stripe from 'angular-stripe'
+
+export default angular.module('payment.stripe', [
+  stripe
+])
+.name
---
+++ stripe/mock.js
@@ -0,0 +1,7 @@
+'use strict'
+
+global.Stripe = {
+  card: {},
+  bankAccount: {},
+  bitcoinReceiver: {}
+}
